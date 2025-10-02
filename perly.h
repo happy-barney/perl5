e2e067075f95b362d72bc7120816b41a7a278e99
Branislav Zahradník <barney@cpan.org>
2025-10-02 08:55:34 +0200

    [parser] Decompose `barestmt` -> `bare_statement_when`

--- perly.h
+++ perly.h
@@ -246,6 +246,6 @@ int yyparse (void);
 
 
 /* Generated from:
- * f914da9595f9dc855e547d44f79e1aa47cb32da036fccb747ff7769f029abd23 perly.y
+ * 46e909c9b88ed290fa4de02ddc0ecbaced4740da0126d567d57095b0019f09b2 perly.y
  * f13e9c08cea6302f0c1d1f467405bd0e0880d0ea92d0669901017a7f7e94ab28 regen_perly.pl
  * ex: set ro ft=c: */
