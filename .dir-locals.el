69f7c7e26a0708036cae4efbda91d3e622139c15
Branislav Zahradník <barney@cpan.org>
2026-08-24 04:41:40 +0200

    ---

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ((nil . ((indent-tabs-mode . nil)))	; all modes
  (cperl-mode . ((cperl-indent-level . 4)))
  (c-mode . ((c-indentation-style . bsd)
-            (c-basic-offset . 4))))
+            (c-basic-offset . 4)))
+ ("perly.y" . ((indent-tabs-mode . t))))
